6e24b910d6722ff179d820cdfacc699070962a10
PRunich <p.runich@oblakogroup.ru>
2022-01-17 18:01:12 +0300

    new version

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,27 +1,30 @@
 package app;
 
-import javax.xml.namespace.QName;
-import java.lang.reflect.Constructor;
+import org.reflections.Reflections;
+
 import java.util.Arrays;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        Class parentClass = Dog.class;
-        int counter = 0;
+        Reflections reflections = new Reflections("app");
+        Class       parentClass = Animal.class;
 
-        while (parentClass != Object.class) {
-            counter += 2;
-            int finalCounter = counter;
+        Set<Class> classes = reflections.getSubTypesOf(parentClass);
 
-            System.out.println(parentClass);
-            System.out.println(Arrays.stream(parentClass.getConstructors()).map(constructor -> " ".repeat(finalCounter) + "- " + constructor).collect(Collectors.joining("\n")));
-            System.out.println(Arrays.stream(parentClass.getFields()).map(field -> " ".repeat(finalCounter) + "+ " +field.getName()).collect(Collectors.joining("\n")));
-            System.out.println(Arrays.stream(parentClass.getDeclaredMethods()).map(field -> " ".repeat(finalCounter) + "* " +field).collect(Collectors.joining("\n")));
-
-            parentClass = parentClass.getSuperclass();
+        classInfo(parentClass, 2);
+        for (Class klass:classes) {
+            classInfo(klass, 4);
         }
+    }
+
+    private static void classInfo(Class parentClass, int countSpaces) {
+        System.out.println(" ".repeat(countSpaces - 2) + "-> " + parentClass);
+        System.out.println(Arrays.stream(parentClass.getConstructors()).map(constructor -> " ".repeat(countSpaces) + "- " + constructor).collect(Collectors.joining("\n")));
+        System.out.println(Arrays.stream(parentClass.getFields()).map(field -> " ".repeat(countSpaces) + "+ " +field.getName()).collect(Collectors.joining("\n")));
+        System.out.println(Arrays.stream(parentClass.getDeclaredMethods()).map(field -> " ".repeat(countSpaces) + "* " +field.getName()).collect(Collectors.joining("\n")));
     }
 }
 
@@ -31,19 +34,26 @@ class Animal {
     public Animal(String name) {
         this.name = name;
     }
+
+    public void speak() {}
+}
+
+class Cat extends Animal {
+    public String catField;
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    public void catMethod() {}
 }
 
 class Dog extends Animal {
-    public int legs;
-    public int eyes;
+    public String dogField;
 
-    public Dog(String name, int legs, int eyes) {
+    public Dog(String name) {
         super(name);
-        this.legs = legs;
-        this.eyes = eyes;
     }
 
-    public String getName() {
-        return name;
-    }
+    public void dogMethod() {}
 }
